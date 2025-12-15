6575bad77466b99b5df8dcaad760d03cbb839a06
Deekshith-AC <deekshith.a.c.4444@gmail.com>
2025-12-15 16:20:29 +0530

    something

--- manna-orru-mission/src/components/Partners.jsx
+++ manna-orru-mission/src/components/Partners.jsx
@@ -10,7 +10,6 @@ export default function Partners(){
           {IMAGES.partnersLogos.map((l,i)=>(
             <div key={l} className="p-4 flex flex-col items-center text-center filter grayscale hover:grayscale-0 transition">
               <img src={l} alt={`partner-${i+1}`} className="w-28 h-28 object-contain" onError={(e)=>{e.currentTarget.src='data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'200\' height=\'200\'><rect width=\'100%\' height=\'100%\' fill=\'%2394c942\'/><text x=\'50%\' y=\'50%\' font-size=\'20\' fill=\'white\' font-family=\'Arial\' text-anchor=\'middle\'>Logo</text></svg>'}}/>
-              <div className="mt-3 text-sm md:text-base lg:text-lg font-semibold text-gray-800">Partner {i+1}</div>
             </div>
           ))}
         </div>
